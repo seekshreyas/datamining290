cdb8e1afe07ee8b2f9e5a2ebe4b7af111bdca7ea
Shreyas <rhodes.unci@gmail.com>
2013-02-25 14:35:03 -0800

    started working on the gini index - getting candidate names as keys

--- code/gini.py
+++ code/gini.py
@@ -15,13 +15,31 @@ receipt_desc, memo_cd, memo_text, form_tp, file_num, tran_id, election_tp) = ran
 # TODO: declare datastructures
 
 ############### Read through files
+
+## defining a dictionary data structure for holding candidate data
+elecDonations = {'candidate' : '', 'donationFrom' :''}
+
+
 for row in csv.reader(fileinput.input()):
     if not fileinput.isfirstline():
         ###
         # TODO: replace line below with steps to save information to calculate
         # Gini Index
-        row[cand_nm], row[contbr_zip]
+        #row[cand_nm], row[contbr_zip]
         ##/
+        # cand = row[cand_nm]
+
+        ## candidate name normalization
+        candName = row[cand_nm].split(',')[0] #take only the last name
+        candName = candName.lower() # convert to lowercase
+
+        if 'candName' in elecDonations:
+        	elecDonations[candName].append(row[contbr_zip])
+        else:
+        	elecDonations[candName] = row[contbr_zip]
+
+
+print elecDonations.keys()
 
 ###
 # TODO: calculate the values below:
